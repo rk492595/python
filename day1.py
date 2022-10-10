413eb9839a18c5b557478b0071be9d492c48926d
rk492595 <rk492595@gmail.com>
2022-10-10 22:46:14 +0530

    new file added

---
+++ day1.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+
+
+# c=print("Welcome for try to search new internship for graphic design\n")
+
+# city=input("Please enter your city\n")
+
+# pet=input("What is your pet name\n")
+
+# print("Your brand name could be " + city + " " + pet)
+
+print(str("45") + str("10"))
---
+++ day3.py
@@ -0,0 +1,132 @@
+#how can we find leap year or not
+# year=int(input("Which year we want to find leap or not"))
+# if year%4==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print("This year is leap year")
+#         else:
+#          print("This is not a leap year")
+#     else:
+#         print("This is leap year")
+# else:
+#  print("This is also a not leap year") 
+
+# conditional statement
+
+# Today 9:47 and 9/04/2022
+
+# conditional statements 
+# year=int(input("please enter year name you want to find leap year or not"))
+# if(year%4==0):
+#     if(year%100==0):
+#         if(year%400==0):
+#             print("Leap year")
+#         else:
+#             print("Not leap year")
+#     else:
+#         print("Leap year")
+# else:
+#     print("Not leap year")
+
+
+# 32
+# print("Welcome to the rollercoaster")
+# height=int(input("What is your height in cm? "))
+# bill=0
+# if height >120:
+#     print("you can ride rollercoaster| ")
+#     age=int(input("What is your age "))
+#     if age < 12:
+#         bill=5
+#         print("Child ticket price are $5")
+#     if age <= 18:
+#         bill=7
+#         print("Youth ticket price are $7")
+#     if age >18:
+#         bill=12
+#         print("Adult ticket price are $12")
+#     want_photos=input("Do you want to photo taken ? Y or N")
+#     if want_photos=="Y":
+#         bill += 3
+#         print("Your final bill is ",bill)
+# else:
+#     print("Sorry, Before you have to grow taller before you can ride:") 
+
+
+# 033
+# pizza_size=input("please enter your pizza size S M or L ")
+# bill=0
+# if pizza_size=="S":
+#     bill=15
+#     print("Small pizza price is $15")
+# elif pizza_size=="M":
+#     bill = 20
+#     print("Medium size pizza is $20")
+# elif pizza_size=="L":
+#     bill = 25
+#     print("Large size pizza price is $25")   
+# paproni=input("Do you want to paproni Y or N")
+# if paproni=="Y":
+#   if pizza_size=="S":
+#       bill +=2
+#       print("Your final bill is ", bill)
+#   else:
+#       bill +=3
+#       print("Your final bill is ",bill)
+# cheese=input("Do you want to cheease please enter Y or N")
+# if cheese=="Y":
+#     bill +=1
+#     print("Your final bill is ", bill)      
+
+
+
+#035
+
+
+
+# question make a love calculater
+
+# print("Welcome to love calculator")
+# name1=input("Please enter your name: ")
+# name2=input("please enter their name: ")
+
+# combined_string=name1 + name2
+# lower_case_string=combined_string.lower()
+
+
+# t = lower_case_string.count("t")
+# r = lower_case_string.count("r")
+# u = lower_case_string.count("u")
+# e = lower_case_string.count("e")
+
+# true = t + r + u + e
+
+# l = lower_case_string.count("l")
+# o = lower_case_string.count("o")
+# v = lower_case_string.count("v")
+# e = lower_case_string.count("e")
+
+# love = l + o + v + e 
+
+# love_score= int(str(true) + str(love))
+# print(love_score)
+
+# if (love_score) < 10 or (love_score) > 90:
+#     print("Your score is ",love_score, " You go together like coke and menter")
+# elif (love_score >= 40) and (love_score <= 50):
+#     print(f"Your love score is {love_score}, You are all right together ")   
+# else:
+#     print(f"your score is {love_score} . ") 
+
+
+# Welcome to Treasure island. Your mission is to find the treasure
+
+print("Welcome to Treasure island. Your mission is to find the treasure")
+a=print(input("In this game where you to move 'left' or 'right' "))
+if a == 'right':
+    print("Game Over")
+if a == 'left':
+    b==print(input("please one option choose 'swim' or 'wait' "))
+
+
+
