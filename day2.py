ee638dc723bc032f585c92bc1a378f0f93697d2e
rk492595 <rk492595@gmail.com>
2022-10-10 22:48:28 +0530

    new file added

---
+++ day2.py
@@ -0,0 +1,37 @@
+
+# ========Two integer sum==========
+# user_input=input("Please enter two integer")
+# a=user_input[0]
+# b=user_input[1]
+# sum=int(a)+int(b)
+# print(sum)
+
+
+
+# find body mass index
+# a=float(input("plese enter your weight"))
+# b=float(input("please enter your height"))
+# c=(a/b**2)
+# d=round(c, 2)
+# print("Body mass index is ", d)
+
+
+
+# a=input("plese enter your weight: ")
+# b=input("please enter your height: ")
+# c=(int(a)/float(b)**2)
+# d=round(c, 2)
+# print("Body mass index is ", d)
+
+#f-string(using of different data type in one line print)
+# score=0
+# height=1.8
+# isWinning=True
+#in line number 31 the code is very complicated to write
+# print("Your score is ",score,"your height is",height, "your are winning in" ,isWinning)
+# #f-string
+# print(f"Your score is {score},your height is {height},you are winning in {isWinning}")
+# print(f"Your score is {score} your height is {height} you are winning in {isWinning}")
+import sys
+a=input("pleaze enter")
+print(sys.getsizeof(a))
